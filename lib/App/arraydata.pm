eed286afefabe3ca83c1448dfac2bc9628f490e7
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-06-07 21:17:53 +0700

    Release v0.001

--- lib/App/arraydata.pm
+++ lib/App/arraydata.pm
@@ -18,6 +18,7 @@ our %argspecopt_module = (
     module => {
         schema => 'perl::arraydata::modname_with_optional_args*',
         cmdline_aliases => {m=>{}},
+        pos => 0,
     },
 );
 
@@ -42,7 +43,7 @@ sub _list_installed {
         (my $mod = $mod0) =~ s/\AArrayData:://;
 
         push @res, {
-            name => $wl,
+            name => $mod,
             path => $mods->{$mod0}{module_path},
         };
      }
@@ -53,7 +54,7 @@ $SPEC{arraydata} = {
     v => 1.1,
     summary => 'Show content of ArrayData modules (plus a few other things)',
     args => {
-        %argspectopt_module,
+        %argspecopt_module,
         action => {
             schema  => ['str*', in=>[
                 'list_installed',
@@ -64,37 +65,42 @@ $SPEC{arraydata} = {
             ]],
             default => 'dump',
             cmdline_aliases => {
-                l => {
-                    summary=>'List installed ArrayData::* modules',
+                L => {
+                    summary=>'List installed ArrayData::*',
                     is_flag => 1,
                     code => sub { my $args=shift; $args->{action} = 'list_installed' },
                 },
-                L => {
-                    summary=>'List ArrayData::* modules on CPAN',
+                C => {
+                    summary=>'List ArrayData::* on CPAN',
                     is_flag => 1,
-                    code => sub { my $args=shift; $args->{action} = 'list_cpan' },
+                    code => sub { my $args=shift; $args->{action} = 'list_installed' },
                 },
-                r => {
+                R => {
                     summary=>'Pick random elements from an ArrayData module',
                     is_flag => 1,
                     code => sub { my $args=shift; $args->{action} = 'pick' },
                 },
-                #s => {
+                #S => {
                 #    summary=>'Show statistics contained in the ArrayData module',
                 #    is_flag => 1,
                 #    code => sub { my $args=shift; $args->{action} = 'stat' },
                 #},
             },
         },
+        detail => {
+            schema => 'bool*',
+            cmdline_aliases => {l=>{}},
+        },
         num => {
-            summary => 'Number of elements to pick (for -r)',
+            summary => 'Number of elements to pick (for -R)',
             schema => 'posint*',
             default => 1,
+            cmdline_aliases => {n=>{}},
         },
-        lcpan => {
-            schema => 'bool',
-            summary => 'Use local CPAN mirror first when available (for -L)',
-        },
+        #lcpan => {
+        #    schema => 'bool',
+        #    summary => 'Use local CPAN mirror first when available (for -C)',
+        #},
     },
     examples => [
     ],
@@ -105,8 +111,14 @@ sub arraydata {
     my $action = $args{action} // 'dump';
 
     if ($action eq 'list_installed') {
-        return [200, "OK", _list_installed()];
+        my @rows;
+        for my $row (@{ _list_installed() }) {
+            push @rows, $args{detail} ? $row : $row->{name};
+        }
+        return [200, "OK", \@rows];
     }
+
+    return [400, "Please specify module"] unless defined $args{module};
 
     require Module::Load::Util;
     my $obj = Module::Load::Util::instantiate_class_with_optional_args(
